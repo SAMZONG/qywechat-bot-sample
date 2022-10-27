5b05473f8199caae413fd151fcf7a3442f4e2939
samzong.lu <samzong.lu@gmail.com>
2022-10-27 17:38:57 +0800

    init project

---
+++ app.py
@@ -0,0 +1,33 @@
+# /usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+"""
+
+Author: samzong.lu
+E-mail: samzong.lu@gmail.com
+
+"""
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return 'Hello World!'
+
+
+@app.route('/qywxbot/callbak', methods=['GET', 'POST'])
+def qywxbot_callbak():
+    data = request.get_json()
+    headers = request.headers
+
+    print(data, '\n', headers)
+
+    return {"code": 200, "message": 'qywxbot_callbak'}
+
+
+if __name__ == '__main__':
+    app.run()
